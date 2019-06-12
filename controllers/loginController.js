b0cdb17e443495dc782ddd70fcc8d60a3f6cec9f
Dan <dan.avraham23@gmail.com>
2019-06-12 23:08:48 +0300

    login works

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -15,17 +15,24 @@ app.controller("loginController", function ($scope, $http, $window) {
         // };
         await $http.post("http://localhost:3000/users/login", $scope.credentials)
             .then(
-                function (response) {
+                    function (response) {
                     if(response.data){
                         console.log(response.data);
+                        $scope.token  = JSON.stringify({
+                            token: response.data["token"],
+                            username: $scope.login_username
+                        });
+                        localStorage.setItem("token", $scope.token);
+                        $window.location.href = "/index.html";
                     }
                     else{
-                        console.log("no data returned")
+                        console.log("something went wrong!");
+                        alert("something went wrong");
                     }
                 },
                 function (response) {
                     console.log(response);
-                    alert("shit!");
+                    alert(response.data);
                 }
             );
     };
