28c6721f5d45ad3f1ba5f57ec056ddb26736754d
Dan <dan.avraham23@gmail.com>
2019-06-12 13:08:47 +0300

    ng-controller connected

--- controller.js
+++ controller.js
@@ -1,8 +1,19 @@
 var main = angular.module("main", ["ngRoute"]);
-main.config(($provide) => {
-    $provide.when("/", {
+main.config(['$routeProvider', function ($routeProvider){
+    $routeProvider.when("/", {
         templateUrl: "contents/home.html",
         controller: "controller",
         controllerAs:"controller"
     })
+    .otherwise({
+        redirectTo: '/'
+    });
+}]);
+main.controller("controller", ($scope, $http, $window) =>{
+    $scope.logged_in = false;
+    $scope.user_name = 'guest'
+    $http({
+        method: 'GET',
+        url: 'http://localhost:3000/poi/getrandomPOI/3'
+    }).then((response) => $scope.random_pois=response.data, (err) =>console.log(err));
 });
